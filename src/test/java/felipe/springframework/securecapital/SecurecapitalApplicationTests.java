d08c1d6196c34b5608073a5fe407a9dc78e68ba5
felipealvarezb1 <felipe.alvarezb@perficient.com>
2023-06-08 09:38:00 -0500

    Initial project

---
+++ src/test/java/felipe/springframework/securecapital/SecurecapitalApplicationTests.java
@@ -0,0 +1,13 @@
+package felipe.springframework.securecapital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
